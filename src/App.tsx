76d1f7b3c6e41e3a0d97e9cb6599cd6b11c81154
Simon <s.zapf@onlineprinters.com>
2022-09-08 14:04:55 +0200

    eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
 
 function App() {
-  const [msg, setMsg] = useState("");
+  const [msg, setMsg] = useState('');
 
   useEffect(() => {
     const sayHello = async () => {
       const data: string = await invoke('greet');
       setMsg(data);
-    }
+    };
 
-    sayHello()
-  }, [])
+    sayHello();
+  }, []);
 
   return (
     <>
-      <h1>{ msg }</h1>
+      <h1>{msg}</h1>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App";
-import store from "./app/store";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './app/store';
 import './styles/styles.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
