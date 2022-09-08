30c7d45ec9710657dcc05af7a1e00b996ddb2bc7
Simon Zapf <simon-zapf@hotmail.de>
2022-09-08 18:02:53 +0200

    add lando commands

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
-import { Scanner } from './lib/scanner';
+import { ChildProcess } from '@tauri-apps/api/shell';
+import { Lando } from './lib/lando';
 
 function App() {
-  const test = async () => {
-    const scanner = new Scanner('/Users/simonzapf/Entwicklung/www/');
-    await scanner.scanDir();
-
-    console.log(scanner.landoEnviorments);
+  const start = async () => {
+    Lando.start('/Users/simonzapf/Entwicklung/www/test').then((std: ChildProcess) => {
+      console.log(std.stdout);
+    });
   };
+
+  const info = async () => {
+    Lando.start('/Users/simonzapf/Entwicklung/www/test').then((std: ChildProcess) => {
+      console.log(std.stdout);
+    });  };
+
+  const stop = async () => {
+    Lando.stop('/Users/simonzapf/Entwicklung/www/test').then((std: ChildProcess) => {
+      console.log(std.stdout);
+    });  };
+
+  const destroy = async () => {
+    Lando.destroy('/Users/simonzapf/Entwicklung/www/test').then((std: ChildProcess) => {
+      console.log(std.stdout);
+    });  };
+
+  const rebuild = async () => {
+    Lando.rebuild('/Users/simonzapf/Entwicklung/www/test').then((std: ChildProcess) => {
+      console.log(std.stdout);
+    });  };
+
+  const restart = async () => {
+    Lando.restart('/Users/simonzapf/Entwicklung/www/test').then((std: ChildProcess) => {
+      console.log(std.stdout);
+    });  };
+
+  const poweroff = async () => {
+    Lando.poweroff().then((std: ChildProcess) => {
+      console.log(std.stdout);
+    });  };
 
   return (
     <>
-      <button onClick={test}>Test</button>
+      <button onClick={start}>Start</button>
+      <button onClick={stop}>Stop</button>
+      <button onClick={info}>Info</button>
+      <button onClick={restart}>Restart</button>
+      <button onClick={rebuild}>Rebuild</button>
+      <button onClick={destroy}>Destroy</button>
+      <button onClick={poweroff}>Poweroff</button>
     </>
   );
 }
--- src/lib/lando.ts
+++ src/lib/lando.ts
@@ -1,7 +1,38 @@
-import { invoke } from '@tauri-apps/api/tauri';
+import { ChildProcess, Command } from '@tauri-apps/api/shell';
 
 export class Lando {
-  public static start(dir: string): Promise<boolean> {
-    return invoke('lando_start', { dir: dir });
+  public static start(dir: string): Promise<ChildProcess> {
+    const command = new Command('run-lando-command', 'start', { cwd: dir });
+    return command.execute();
+  }
+
+  public static info(dir: string): Promise<ChildProcess> {
+    const command = new Command('run-lando-command', 'info', { cwd: dir });
+    return command.execute();
+  }
+
+  public static stop(dir: string): Promise<ChildProcess> {
+    const command = new Command('run-lando-command', 'stop', { cwd: dir });
+    return command.execute();
+  }
+
+  public static destroy(dir: string): Promise<ChildProcess> {
+    const command = new Command('run-lando-command', ['destroy', '-y'], { cwd: dir });
+    return command.execute();
+  }
+
+  public static restart(dir: string): Promise<ChildProcess> {
+    const command = new Command('run-lando-command', 'restart', { cwd: dir });
+    return command.execute();
+  }
+
+  public static rebuild(dir: string): Promise<ChildProcess> {
+    const command = new Command('run-lando-command', ['rebuild', '-y'], { cwd: dir });
+    return command.execute();
+  }
+
+  public static poweroff(): Promise<ChildProcess> {
+    const command = new Command('run-lando-command', 'poweroff');
+    return command.execute();
   }
 }
