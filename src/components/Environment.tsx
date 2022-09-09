5c8da3c992d409f97f8e375673b98a7126d960d9
Simon <s.zapf@onlineprinters.com>
2022-09-09 14:27:29 +0200

    show lando info

--- src/components/Environment.tsx
+++ src/components/Environment.tsx
@@ -4,15 +4,19 @@ import CardActions from '@mui/material/CardActions/CardActions';
 import CardContent from '@mui/material/CardContent/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
+import Collapse from '@mui/material/Collapse/Collapse';
 import Icon from '@mui/material/Icon/Icon';
 import IconButton from '@mui/material/IconButton/IconButton';
 import Tooltip from '@mui/material/Tooltip/Tooltip';
 import Typography from '@mui/material/Typography/Typography';
+import { ChildProcess } from '@tauri-apps/api/shell';
 import { useState } from 'react';
 import { Lando } from '../lib/lando';
 import { Notification } from '../lib/notification';
 import { LandoEnv } from '../models/lando-env';
 import UrlModal from './UrlModal';
+import JSONPretty from 'react-json-pretty';
+import * as dJSON from 'dirty-json';
 
 interface EnvironmentProps {
     env: LandoEnv;
@@ -22,6 +26,26 @@ function Environment({ env }: EnvironmentProps) {
     const notifications = new Notification();
 
     const [loading, setLoading]: [boolean, (loading: boolean) => void] = useState(false);
+    const [expanded, setExpanded]: [boolean, (expanded: boolean) => void] = useState(false);
+    const [landoInfo, setLandoInfo]: [string, (landoInfo: string) => void] = useState('');
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+        if (!expanded) {
+            loadInfo();
+        }
+    };
+
+    const loadInfo = () => {
+        if (landoInfo === '') {
+            setLoading(true);
+        }
+        Lando.info(env.path).then((out: ChildProcess) => {
+            const json = JSON.stringify(dJSON.parse(out.stdout));
+            setLandoInfo(json);
+            setLoading(false);
+        });
+    };
 
     const onStart = () => {
         notifications.send('Starting ' + env.name);
@@ -75,20 +99,20 @@ function Environment({ env }: EnvironmentProps) {
     return (
         <Card sx={{ display: 'flex', flexDirection: 'row' }}>
             <Box sx={{ display: 'flex', flexDirection: 'column', width: '80%' }}>
-                <CardHeader 
-                title={
-                    <Typography component='div' variant='h5'>
-                        {env.name}
-                    </Typography>
-                }
-                subheader={env.path}>
+                <CardHeader
+                    title={
+                        <Typography component='div' variant='h5'>
+                            {env.name}
+                        </Typography>
+                    }
+                    subheader={env.path}>
                 </CardHeader>
                 {/* readd when check running envs is implmented
 
                 <CardContent>
                     <Typography color='text.secondary' >Running</Typography>
                 </CardContent> */}
-                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', pl: 1, pb: 1 }}>
                     <CardActions>
                         <UrlModal path={env.path} />
                         {env.running ? (
@@ -120,12 +144,26 @@ function Environment({ env }: EnvironmentProps) {
                                 <Icon>delete</Icon>
                             </IconButton>
                         </Tooltip>
-                        <Tooltip title='Show Output'>
-                            <IconButton>
-                                <Icon>expand_more</Icon>
-                            </IconButton>
-                        </Tooltip>
+                        {!expanded ? (
+                            <Tooltip title='Show Info'>
+                                <IconButton onClick={handleExpandClick}>
+                                    <Icon>expand_more</Icon>
+                                </IconButton>
+                            </Tooltip>
+                        ) : (
+                            <Tooltip title='Hide Info'>
+                                <IconButton onClick={handleExpandClick}>
+                                    <Icon>expand_less</Icon>
+                                </IconButton>
+                            </Tooltip>
+                        )
+                        }
                     </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent sx={{ fontSize: '12px' }}>
+                            <JSONPretty data={landoInfo}></JSONPretty>
+                        </CardContent>
+                    </Collapse>
                 </Box>
             </Box>
             <Box sx={{
